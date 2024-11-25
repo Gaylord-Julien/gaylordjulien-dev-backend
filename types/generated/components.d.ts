ac4256941a31ae4186cec77c67f50bb5d8236be0
zharkan <contact@gaylordjulien.dev>
2024-11-25 14:48:53 +0100

    chore: bump strapi version

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,30 +1,41 @@
-import type { Struct, Schema } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface SharedSeo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_seos';
+export interface HomepageHeader extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_headers';
   info: {
-    displayName: 'seo';
-    icon: 'search';
+    description: '';
+    displayName: 'header';
   };
   attributes: {
-    metaTitle: Schema.Attribute.String &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMaxLength<{
-        maxLength: 60;
-      }>;
-    metaDescription: Schema.Attribute.String &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMaxLength<{
-        minLength: 50;
-        maxLength: 160;
-      }>;
-    metaImage: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
-    metaSocial: Schema.Attribute.Component<'shared.meta-social', true>;
-    keywords: Schema.Attribute.Text;
-    metaRobots: Schema.Attribute.String;
-    structuredData: Schema.Attribute.JSON;
-    metaViewport: Schema.Attribute.String;
-    canonicalURL: Schema.Attribute.String;
+    content: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface HomepageIntro extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_intros';
+  info: {
+    description: '';
+    displayName: 'intro';
+  };
+  attributes: {
+    content: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images'>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface HomepageTrust extends Struct.ComponentSchema {
+  collectionName: 'components_homepage_trusts';
+  info: {
+    description: '';
+    displayName: 'trust';
+  };
+  attributes: {
+    description: Schema.Attribute.Blocks;
+    screenshot: Schema.Attribute.Media<'images', true>;
+    title: Schema.Attribute.String;
+    website: Schema.Attribute.String;
   };
 }
 
@@ -35,6 +46,12 @@ export interface SharedMetaSocial extends Struct.ComponentSchema {
     icon: 'project-diagram';
   };
   attributes: {
+    description: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 65;
+      }>;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
     socialNetwork: Schema.Attribute.Enumeration<['Facebook', 'Twitter']> &
       Schema.Attribute.Required;
     title: Schema.Attribute.String &
@@ -42,62 +59,45 @@ export interface SharedMetaSocial extends Struct.ComponentSchema {
       Schema.Attribute.SetMinMaxLength<{
         maxLength: 60;
       }>;
-    description: Schema.Attribute.String &
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    canonicalURL: Schema.Attribute.String;
+    keywords: Schema.Attribute.Text;
+    metaDescription: Schema.Attribute.String &
       Schema.Attribute.Required &
       Schema.Attribute.SetMinMaxLength<{
-        maxLength: 65;
+        maxLength: 160;
+        minLength: 50;
       }>;
-    image: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
-  };
-}
-
-export interface HomepageTrust extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_trusts';
-  info: {
-    displayName: 'trust';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    website: Schema.Attribute.String;
-    screenshot: Schema.Attribute.Media<'images', true>;
-    description: Schema.Attribute.Blocks;
-  };
-}
-
-export interface HomepageIntro extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_intros';
-  info: {
-    displayName: 'intro';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    image: Schema.Attribute.Media<'images'>;
-    content: Schema.Attribute.Text;
-  };
-}
-
-export interface HomepageHeader extends Struct.ComponentSchema {
-  collectionName: 'components_homepage_headers';
-  info: {
-    displayName: 'header';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    content: Schema.Attribute.Text;
+    metaImage: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+    metaRobots: Schema.Attribute.String;
+    metaSocial: Schema.Attribute.Component<'shared.meta-social', true>;
+    metaTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaViewport: Schema.Attribute.String;
+    structuredData: Schema.Attribute.JSON;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'shared.seo': SharedSeo;
-      'shared.meta-social': SharedMetaSocial;
-      'homepage.trust': HomepageTrust;
-      'homepage.intro': HomepageIntro;
       'homepage.header': HomepageHeader;
+      'homepage.intro': HomepageIntro;
+      'homepage.trust': HomepageTrust;
+      'shared.meta-social': SharedMetaSocial;
+      'shared.seo': SharedSeo;
     }
   }
 }
