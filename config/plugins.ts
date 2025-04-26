2c7cb4a4d72fa2980fad909a23a4a72ed7c77a1c
zharkan <contact@gaylordjulien.dev>
2025-04-26 15:51:03 +0200

    feat: add placeholder plugin configuration and placeholder field to song schema

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,25 +1,36 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        baseUrl: env("MINIO_PUBLIC_ENDPOINT"),
-        s3Options: {
-          credentials: {
-            accessKeyId: env("MINIO_ROOT_USER"),
-            secretAccessKey: env("MINIO_ROOT_PASSWORD"),
-          },
-          endpoint: env("MINIO_PRIVATE_ENDPOINT"),
-          region: env("MINIO_REGION"),
-          forcePathStyle: true,
-          params: {
-            Bucket: env("MINIO_BUCKET"),
-          },
+module.exports = ({env}) => ({
+    upload: {
+        config: {
+            provider: "aws-s3",
+            providerOptions: {
+                baseUrl: env("MINIO_PUBLIC_ENDPOINT"),
+                s3Options: {
+                    credentials: {
+                        accessKeyId: env("MINIO_ROOT_USER"),
+                        secretAccessKey: env("MINIO_ROOT_PASSWORD"),
+                    },
+                    endpoint: env("MINIO_PRIVATE_ENDPOINT"),
+                    region: env("MINIO_REGION"),
+                    forcePathStyle: true,
+                    params: {
+                        Bucket: env("MINIO_BUCKET"),
+                    },
+                },
+            },
         },
-      },
     },
-  },
-  seo: {
-    enabled: true,
-  },
+    seo: {
+        enabled: true,
+    },
+    "strapi-plugin-placeholder": {
+        enabled: true,
+        resolve: "./node_modules/strapi-plugin-placeholder",
+        config: {
+            endpoint: env("MINIO_ENDPOINT"),
+            accessKey: env("MINIO_ACCESS_KEY"),
+            secretKey: env("MINIO_SECRET_KEY"),
+            bucket: env("MINIO_BUCKET"),
+            forcePathStyle: true,
+        },
+    },
 });
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -777,6 +777,7 @@ export interface PluginUploadFile extends Struct.CollectionTypeSchema {
       Schema.Attribute.Private;
     mime: Schema.Attribute.String & Schema.Attribute.Required;
     name: Schema.Attribute.String & Schema.Attribute.Required;
+    placeholder: Schema.Attribute.Text;
     previewUrl: Schema.Attribute.String;
     provider: Schema.Attribute.String & Schema.Attribute.Required;
     provider_metadata: Schema.Attribute.JSON;
